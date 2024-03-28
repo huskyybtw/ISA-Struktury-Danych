cf3ac82756cbda17490925554b950d55aa2ea950
huskybtw <marekkocikw@gmail.com>
2024-03-28 16:24:20 +0100

    missing return in doublylist->removerandom

--- main.cpp
+++ main.cpp
@@ -5,12 +5,6 @@
 
 
 int main() {
-    DoublyList<int>* mylist = new DoublyList<int>();
-
-
-    std::cout<< "WIELKOSC LISTY " ;
-    std::cout<< mylist->getSize() << std::endl;
-    delete mylist;
     return 0;
 }
 
