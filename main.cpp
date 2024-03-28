ee3c8509c4a4d3bbc0aed968b5b27291db14c777
huskybtw <marekkocikw@gmail.com>
2024-03-28 15:58:50 +0100

    DOUBLYLIST -CHYBA NAPRAWIONY BLAD W RemoveRANDOM (teraz przestawia wskaznik na element do usuniecia i funckja radzi sobie jak sa tylko dwa elementy)

--- main.cpp
+++ main.cpp
@@ -7,17 +7,16 @@
 int main() {
     DoublyList<int>* mylist = new DoublyList<int>();
 
-    randomTest(0,3,*mylist);
-    randomTest(1,2,*mylist);
-    randomTest(2,20,*mylist);
-    //randomTest(4,25,*mylist); // dziala wpelni
-    //randomTest(5,19,*mylist); // 5  dziala jesli nie dodajemy 2
-    //randomTest(3,4999,*mylist); // 5 I 2 wydaja sie nie dzialac i psuc inne funckje
+    randomTest(0,1000,*mylist);
+    randomTest(1,1000,*mylist);
+    randomTest(2,100000,*mylist);
+    randomTest(4,1000,*mylist); // dziala wpelni
+    randomTest(5,1500,*mylist); // 5  dziala jesli nie dodajemy 2
+    randomTest(3,499900,*mylist); // 5 I 2 wydaja sie nie dzialac i psuc inne funckje
 
 
     std::cout<< "WIELKOSC LISTY " ;
     std::cout<< mylist->getSize() << std::endl;
-    mylist->printList();
     delete mylist;
     return 0;
 }
