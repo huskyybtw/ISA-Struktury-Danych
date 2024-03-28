bb14f6622fcba8939b16cae394b618a88737a1ec
huskybtw <marekkocikw@gmail.com>
2024-03-28 15:34:36 +0100

    DOUBLYLIST -NAPRAWIONY BLAD W ADD RANDOM (ZLA KOLEJNOSC PODMIENIALEM TEMP->NEXT A POTEM TEMP->PREV->NEXT

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@
 int main() {
     DoublyList<int>* mylist = new DoublyList<int>();
 
-    randomTest(0,5,*mylist);
-    randomTest(1,5,*mylist);
-    randomTest(2,5,*mylist);
-    randomTest(4,25,*mylist);
-    /*randomTest(5,1,*mylist); // 5 I 3 NIE DZIALAJA
-    randomTest(3,1,*mylist); // 5 I 3 NIE DZIALAJA
-    */
+    randomTest(0,3,*mylist);
+    randomTest(1,2,*mylist);
+    randomTest(2,20,*mylist);
+    //randomTest(4,25,*mylist); // dziala wpelni
+    //randomTest(5,19,*mylist); // 5  dziala jesli nie dodajemy 2
+    //randomTest(3,4999,*mylist); // 5 I 2 wydaja sie nie dzialac i psuc inne funckje
+
 
     std::cout<< "WIELKOSC LISTY " ;
     std::cout<< mylist->getSize() << std::endl;
+    mylist->printList();
     delete mylist;
     return 0;
 }
