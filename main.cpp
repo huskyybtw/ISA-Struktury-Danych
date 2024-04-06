29b5c79f94c8e1a0b7e9fbaf371890131e623d05
Sabaath0 <bart221214@poczta.fm>
2024-04-06 13:20:44 +0200

    TailList

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 
 
 int main() {
+
+    TailList<int>* lista = new TailList<int>();
+    randomTest(0,10,*lista);
+    randomTest(1,8,*lista);
+   // randomTest(5,1,*lista);
+    //randomTest(2,10000,*lista);
+    //randomTest(3,9997,*lista);
+    lista->printList();
+    delete lista;
+
     return 0;
 }
 
